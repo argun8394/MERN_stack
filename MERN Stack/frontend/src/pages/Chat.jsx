5d95d5bf1698d1224dcda9194378d801c1f0ec9c
argun8394 <argundoganali@gmail.com>
2024-09-30 00:21:56 +0300

    update Chat component

--- MERN Stack/frontend/src/pages/Chat.jsx
+++ MERN Stack/frontend/src/pages/Chat.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import useGetQuestions from "../hooks/useGetQuestions";
+import useSubmittedAnswer from '../hooks/useSubmittedAnswer';
+
 
 const API = import.meta.env.VITE_API_URL;
 
@@ -13,59 +15,14 @@ const Chat = () => {
     answer: '',
   });
 
+  const {getQuestions} = useGetQuestions(setSuccess, setError, setQuestions, success, error )
+  const {handleSubmit} = useSubmittedAnswer(submittedAnswers, setSubmittedAnswers, formData, setFormData, setSuccess, setError, error)
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
-  };
-  const token = localStorage.getItem('accessToken');
-  const getQuestions = async () => {
-    try {
-      const response = await axios.get(`${API}/questions`, {
-        headers: {
-          'Authorization': 'Bearer ' + token
-        }
-      });
-
-      if (response.status == 200) {
-        setSuccess('Get data successfull!');
-        setError('');
-        setQuestions(response.data.data)
-        console.log(success)
-        console.log(questions)
-      }
-    } catch (err) {
-      setError('Failed get Data.');
-      setSuccess('');
-      console.log(error)
-    }
-  }
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post(`${API}/answers`, formData, {
-        headers: {
-          'Authorization': 'Bearer ' + token
-        }
-      });
-
-      if (response.status === 201) {
-        setSubmittedAnswers([
-          ...submittedAnswers,
-          { answer: formData.answer }
-        ]);    
-
-        setSuccess('Answer submitted successfully!');
-        setError('');
-        setFormData({ answer: '' });
-      }
-    } catch (err) {
-      setError('Failed to submit answer. ' + err.message);
-      setSuccess('');
-      console.log(error);
-    }
   };
 
   useEffect(() => {
@@ -114,7 +71,7 @@ const Chat = () => {
                 value={formData.answer}
                 onChange={handleChange}
                 required
-                className="border rounded-md px-2 py-1"
+                className="border rounded-md px-2 py-1 text-black"
                 placeholder='your answer...'
               />
 
