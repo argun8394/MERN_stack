8e265bb22ee16c00d75855199957853d31ede0b3
argun8394 <argundoganali@gmail.com>
2024-10-01 22:32:25 +0300

    update answer controller

--- MERN Stack/backend/src/controllers/answer.js
+++ MERN Stack/backend/src/controllers/answer.js
@@ -30,4 +30,35 @@ module.exports = {
     });
   },
 
+  read: async (req, res) => {
+
+    const data = await Answer.findOne({ _id: req.params.id })
+
+    res.status(200).send({
+      error: false,
+      data
+    })
+  },
+
+  update: async (req, res) => {
+
+    const data = await Answer.updateOne({ _id: req.params.id }, req.body)
+
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Answer.findOne({ _id: req.params.id })
+    })
+  },
+
+  delete: async (req, res) => {
+
+    const data = await Answer.deleteOne({ _id: req.params.id })
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data
+    })
+  },
+
 }
