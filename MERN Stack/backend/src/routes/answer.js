9ee0c754396761d515f8588d0d3d45c9804c9d91
argun8394 <argundoganali@gmail.com>
2024-10-01 23:09:50 +0300

    update answer route

--- MERN Stack/backend/src/routes/answer.js
+++ MERN Stack/backend/src/routes/answer.js
@@ -4,17 +4,22 @@ const router = require('express').Router()
 /* -------------------------------------------------------*/
 // routes/answer:
 
+const { isLogin } = require('../middlewares/permissions')
 const answer = require('../controllers/answer')
+
+// URL: /answers
+
+router.use(isLogin) 
 
 router.route('/')
     .get(answer.list)
     .post(answer.create)
 
 router.route('/:id')
-    // .get(answer.read)
-    // .put(answer.update)
-    // .patch(answer.update)
-    // .delete(answer.delete)
+    .get(answer.read)
+    .put(answer.update)
+    .patch(answer.update)
+    .delete(answer.delete)
 
     /* ------------------------------------------------------*/
     module.exports = router
